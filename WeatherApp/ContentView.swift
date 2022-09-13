2a119b5639a3c49a935840743288873ec1167222
Majid <majid@Majids-Mac-mini.local>
2022-09-13 09:00:40 +0430

    adding base project

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Majid on 13/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
